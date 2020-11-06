0b14477e1ebaa4f0aa28b124934e3ddf4a1757b1
piggy-devil <muupaza@gmail.com>
2020-11-06 14:53:21 +0700

    add AdminLTE3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function () {
+    // All the admin routes will be defined here
+    Route::get('dashboard', 'AdminController@dashboard');
+});
